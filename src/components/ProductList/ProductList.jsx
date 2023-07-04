2890185e6fd536e81d8c2807f4f549acfc816037
Татьяна <isskova.tatyana@yandex.ru>
2023-07-04 23:19:41 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './ProductList.css'
 const ProductList = () => {
     return (
-        <div>
-            ProductList
+        <div className={'list'}>
+            {}
         </div>
     );
 };
