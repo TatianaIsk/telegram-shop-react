ed376ff9780d68a3b598bbd17f25f66b705ac161
Татьяна <isskova.tatyana@yandex.ru>
2023-07-04 15:25:29 +0300

    init

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,52 @@
+import React, {useEffect, useState} from 'react';
+import './Form.css'
+import {useTelegram} from "../../hooks/useTelegram";
+const Form = () => {
+    const [login, setLogin] = useState('')
+    const [password, setPassword] = useState('')
+    const {tg} = useTelegram()
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Отправить данные'
+        })
+    }, [])
+
+    useEffect(() => {
+        if (!login || !password) {
+            tg.MainButton.hide()
+        } else {
+            tg.MainButton.show()
+        }
+    }, [login, password])
+
+    const onChangeLogin = (e) => {
+        setLogin(e.target.value)
+    }
+
+    const onChangePassword = (e) => {
+        setPassword(e.target.value)
+    }
+
+    return (
+        <div className={'form'}>
+            <h3>Введите данные</h3>
+            <input
+                className={'input'}
+                type='text'
+                placeholder={'логин'}
+                value={login}
+                onChange={onChangeLogin}
+            />
+            <input
+                className={'input'}
+                type='password'
+                placeholder={'пароль'}
+                value={password}
+                onChange={onChangePassword}
+            />
+        </div>
+    );
+};
+
+export default Form;
