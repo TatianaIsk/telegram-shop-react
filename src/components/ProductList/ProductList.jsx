c51515d2ca5b97495809f11ced89065159483b57
Татьяна <isskova.tatyana@yandex.ru>
2023-07-05 17:16:36 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -15,34 +15,34 @@ const products = [
     {id: 9, title: 'Латте', img: './../../assets/coffee9.jpg', price: 260}
 ]
 
-const getTotalPrice = (items) => {
+const getTotalPrice = (items = []) => {
     return items.reduce((acc, item) => {
         return acc += item.price
     }, 0)
 }
 
 const ProductList = () => {
-    const [addedItem, setAddedItem] = useState([])
+    const [addedItems, setAddedItems] = useState([]);
     const {tg} = useTelegram()
 
     const onAdd = (product) => {
-        const alreadyAdded = addedItem.find(item => item.id === product.id)
-        let newItem = []
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        let newItems = [];
 
-        if (alreadyAdded) {
-            newItem = addedItem.filter(item => item.id !== product.id)
+        if(alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id);
         } else {
-            newItem = {...addedItem, product}
+            newItems = [...addedItems, product];
         }
 
-        setAddedItem(newItem)
+        setAddedItems(newItems)
 
-        if (newItem.length === 0) {
-            tg.MainButton.hide()
+        if(newItems.length === 0) {
+            tg.MainButton.hide();
         } else {
-            tg.MainButton.show()
-            tg.setParams({
-                text: `Купить: ${getTotalPrice(newItem)}`
+            tg.MainButton.show();
+            tg.MainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
             })
         }
     }
