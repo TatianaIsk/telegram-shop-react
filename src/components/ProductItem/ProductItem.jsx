994fc27687805c00b1eed23003ae456c9968ed6b
Татьяна <isskova.tatyana@yandex.ru>
2023-07-05 17:35:38 +0300

    init

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -8,7 +8,7 @@ const ProductItem = ({product, onAdd}) => {
 
     return (
         <div className={'product'} >
-            <img src={product.img} />
+            <img src={product.img} className={'img'}/>
             <div className={'title'}>{product.title}</div>
             <div className={'price'}>
                 <span>Стоимость: <b>{product.price}</b></span>
---
+++ src/products.js
@@ -0,0 +1,21 @@
+import img1 from './assets/coffee1.jpg'
+import img2 from './assets/coffee2.jpg'
+import img3 from './assets/coffee3.jpg'
+import img4 from './assets/coffee4.jpg'
+import img5 from './assets/coffee5.jpg'
+import img6 from './assets/coffee6.jpg'
+import img7 from './assets/coffee7.jpg'
+import img8 from './assets/coffee8.jpg'
+import img9 from './assets/coffee9.jpg'
+
+export const products = [
+    {id: 1, title: 'Эспрессо', img: img1, price: 200},
+    {id: 2, title: 'Макиато', img: img2, price: 240},
+    {id: 3, title: 'Моккачино', img: img3, price: 250},
+    {id: 4, title: 'Ристретто', img: img4, price: 210},
+    {id: 5, title: 'Фраппе', img: img5, price: 270},
+    {id: 6, title: 'Капучино', img: img6, price: 250},
+    {id: 7, title: 'Гляссе', img: img7, price: 270},
+    {id: 8, title: 'Американо', img: img8, price: 220},
+    {id: 9, title: 'Латте', img: img9, price: 260}
+]
